985b5fdac4a6face3a7501e8acfd822c23467a7b
idlviv <passpass1>
2017-11-27 19:16:51 +0200

    !

--- client.js
+++ client.js
@@ -2,6 +2,6 @@ let httpGet = require('./promise');
 
 httpGet('yo')
   .then(
-    response => console.log('Fulfilled', response),
+    response => console.log('Fulfilled!', response),
     error => console.log('Rejected:', error)
   );
